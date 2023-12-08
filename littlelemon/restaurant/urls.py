9bcc19daf3ad2df47b1330be3f59bbb8cca49003
diep sang <diepqsang13@gmail.com>
2023-12-09 00:08:23 +0700

    Install djangorestframeword Build REST API for Menu and Booking model

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
+from rest_framework import routers
+
 from . import views
 
 
+router = routers.DefaultRouter()
+router.register(r'table', views.BookingViewSet)
+
+
 urlpatterns = [
-	path('', views.index, name='home')
+	path('', views.index, name='home'),
+	path('menu/', views.MenuItemView.as_view()),
+	path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
